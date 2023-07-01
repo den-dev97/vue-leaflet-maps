3e19c3e801e637e7be7a547da57d0ee9fdd1ab2b
den <khausden@gmail.com>
2023-07-01 12:24:25 +1000

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <div>
+      <AgGridVue
+          :columnDefs="columnDefs"
+          :rowData="rowData"
+          class="ag-theme-alpine"
+      />
+    </div>
+
+  </div>
+</template>
+
+<script>
+import { AgGridVue } from "ag-grid-vue";
+
+
+export default {
+  name: 'App',
+  components: {
+    AgGridVue
+  },
+  data: function () {
+    return {
+      columnDefs: ['qwe', 'asd', 'zxc'],
+      rowData: ['qwe', 'asd', 'zxc']
+    }
+  }
+
+}
+</script>
+
+<style>
+@import "~ag-grid-community/dist/styles/ag-grid.css";
+@import "~ag-grid-community/dist/styles/ag-theme-alpine.css";
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
